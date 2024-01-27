e354f5d197d65ea7ad959733bc4bc00f65d76bdc
AyushHirani <ayushhirani4797@gmail.com>
2024-01-27 15:50:29 -0500

    something

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import { auth } from "express-openid-connect";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { MongoClient, ServerApiVersion } from "mongodb";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.RANDOM,
+    baseURL: 'https://timeless-memories-a264.onrender.com',
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: 'https://dev-5a6kmxh5c8c7ujdo.us.auth0.com'
+  };
 const uri = process.env.URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,6 +25,8 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+app.use(auth(config));
 
 async function run() {
   try {
@@ -40,8 +51,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // });
 
-app.get("/create", (req,res) => {
-    res.sendFile(__dirname + "/views/create.html");
+app.get('/create', (req, res) => {
+    var isUser = req.oidc.isAuthenticated() ? 'Yes' : 'No';
+    if(isUser){ 
+        res.sendFile(__dirname + "/views/create.html");
+    }else{
+        res.send("Enter Correct Password.");
+    }
 });
 
 app.get("/", (req,res) => {
