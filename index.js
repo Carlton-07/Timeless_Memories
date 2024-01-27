d7df099be9aca96d7ed44e4a0fe34bb82fa90b25
AyushHirani <ayushhirani4797@gmail.com>
2024-01-27 09:46:41 -0500

    one more

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 import express from "express";
-import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://tmmem:UofTHacks11@tmmem.xc9ioh7.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(error){
+    console.error(error);
+  }
+}
+run().catch(console.dir);
+
 const app = express();
 const port = 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(port, () => {
