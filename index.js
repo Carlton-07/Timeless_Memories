3126ea796a9e624ef62db363465b216fe84bf75b
AyushHirani <ayushhirani4797@gmail.com>
2024-01-26 23:03:09 -0500

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req,res) => {
-    res.send("Hello, World!");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
